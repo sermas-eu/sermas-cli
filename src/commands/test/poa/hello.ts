07770af9198589619cadee9ec83d50f5c43474f9
Kanthavel Pasupathipillai <kanthavel.pasupathipillai@spindox.it>
2025-01-13 15:05:50 +0100

    Implemented say hello test

---
+++ src/commands/test/poa/hello.ts
@@ -0,0 +1,26 @@
+import { Command } from "commander";
+import { Browser } from "../../../libs/testing/browser";
+import { fail } from "../../../libs/util";
+import { CommandParams } from "../../../libs/dto/cli.dto";
+
+export default {
+  setup: async (command: Command) => {
+    command.description("Say hello to Emma");
+  },
+
+  run: async ({ flags }: CommandParams) => {
+    const baseUrl = flags.baseurl || "http://localhost:8080";
+    const pace = parseInt(flags.pace) || 0;
+    const browser: Browser = new Browser(`${baseUrl}/poa`, pace);
+    let reply: string;
+    try {
+      await browser.initSession();
+      reply = await browser.talkToAvatar("Ciao, Emma! Come stai?");
+    } finally {
+      await browser.quit();
+    }
+    if (!reply) fail("No reply from Emma");
+    if (!reply.includes("bene")) fail("Emma is not happy");
+    return [];
+  },
+};

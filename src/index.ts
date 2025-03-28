aedab79e466b3073fe4a2fc4c889655bb699d0bf
Kanthavel Pasupathipillai <kanthavel.pasupathipillai@spindox.it>
2025-03-28 10:25:25 +0100

    Fixed bug (CLI not initialized)

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,8 @@ import logger from "./libs/logger";
 
 const cliProgram = new CliProgram();
 const logStack = (e) => logger.error(e.stack);
-cliProgram.init().catch(logStack);
-cliProgram.parse().catch(logStack);
+cliProgram
+  .init()
+  .catch(logStack)
+  .then(() => cliProgram.parse())
+  .catch(logStack);
